a95b43d7779ee19a9e9db353919955a521ed2c07
benjaminwan <benjaminwan@163.com>
2022-10-14 11:10:01 +0800

    删除vs2017编译环境，windows版增加mt选项

--- build-ncnn-cpu-vs2019.bat
+++ build-ncnn-cpu-vs2019.bat
@@ -6,17 +6,27 @@ SETLOCAL EnableDelayedExpansion
 
 for /f "Delims=" %%x in (ncnn_cmake_options.txt) do set OPTIONS=!OPTIONS!%%x
 
-call :cmakeParams "Visual Studio 16 2019" "v142" "x64"
-call :cmakeParams "Visual Studio 16 2019" "v142" "Win32"
+call :cmakeParams "Visual Studio 16 2019" "v142" "x64" "md"
+call :cmakeParams "Visual Studio 16 2019" "v142" "Win32" "md"
+call :cmakeParams "Visual Studio 16 2019" "v142" "x64" "mt"
+call :cmakeParams "Visual Studio 16 2019" "v142" "Win32" "mt"
 
 GOTO:EOF
 
 :cmakeParams
-mkdir "build-%~2-%~3-cpu"
-pushd "build-%~2-%~3-cpu"
+mkdir "build-%~2-%~3-%~4-cpu"
+pushd "build-%~2-%~3-%~4-cpu"
+if "%~4" == "mt" (
+    set STATIC_CRT_ENABLED=ON
+)^
+else (
+    set STATIC_CRT_ENABLED=OFF
+)
+
 cmake -G "%~1" -T "%~2,host=x64" -A "%~3" -DCMAKE_INSTALL_PREFIX=install ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DNCNN_VULKAN=OFF ^
+  -DNCNN_BUILD_WITH_STATIC_CRT=%STATIC_CRT_ENABLED% ^
   %OPTIONS% ^
   ..
 cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
--- build-ncnn-gpu-vs2019.bat
+++ build-ncnn-gpu-vs2019.bat
@@ -6,17 +6,27 @@ SETLOCAL EnableDelayedExpansion
 
 for /f "Delims=" %%x in (ncnn_cmake_options.txt) do set OPTIONS=!OPTIONS!%%x
 
-call :cmakeParams "Visual Studio 16 2019" "v142" "x64"
-call :cmakeParams "Visual Studio 16 2019" "v142" "Win32"
+call :cmakeParams "Visual Studio 16 2019" "v142" "x64" "md"
+call :cmakeParams "Visual Studio 16 2019" "v142" "Win32" "md"
+call :cmakeParams "Visual Studio 16 2019" "v142" "x64" "mt"
+call :cmakeParams "Visual Studio 16 2019" "v142" "Win32" "mt"
 
 GOTO:EOF
 
 :cmakeParams
-mkdir "build-%~2-%~3-gpu"
-pushd "build-%~2-%~3-gpu"
+mkdir "build-%~2-%~3-%~4-gpu"
+pushd "build-%~2-%~3-%~4-gpu"
+if "%~4" == "mt" (
+    set STATIC_CRT_ENABLED=ON
+)^
+else (
+    set STATIC_CRT_ENABLED=OFF
+)
+
 cmake -G "%~1" -T "%~2,host=x64" -A "%~3" -DCMAKE_INSTALL_PREFIX=install ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DNCNN_VULKAN=ON ^
+  -DNCNN_BUILD_WITH_STATIC_CRT=%STATIC_CRT_ENABLED% ^
   %OPTIONS% ^
   ..
 cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
